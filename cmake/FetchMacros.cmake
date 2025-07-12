64aa98531b9edb83e00be655d2857eb3d442943f
Xander <berten.xander@gmail.com>
2025-07-12 15:41:19 +0200

    Change: Reduce ASSIMP Importers

--- cmake/FetchMacros.cmake
+++ cmake/FetchMacros.cmake
@@ -120,27 +120,110 @@ macro(FETCH_ASSIMP depsDir)
     set(ASSIMP_INSTALL_PDB OFF)
     set(ASSIMP_BUILD_ASSIMP_VIEW OFF)
     set(ASSIMP_BUILD_IFC_IMPORTER OFF)
+    set(ASSIMP_BUILD_X3D_IMPORTER OFF)
+    set(ASSIMP_BUILD_AMF_IMPORTER OFF)
+    set(ASSIMP_BUILD_3DS_IMPORTER OFF)
+    set(ASSIMP_BUILD_AC_IMPORTER OFF)
+    set(ASSIMP_BUILD_ASE_IMPORTER OFF)
+    set(ASSIMP_BUILD_ASSBIN_IMPORTER OFF)
+    set(ASSIMP_BUILD_B3D_IMPORTER OFF)
+    set(ASSIMP_BUILD_D3D_IMPORTER OFF)
+    set(ASSIMP_BUILD_BVH_IMPORTER OFF)
+    set(ASSIMP_BUILD_COLLADA_IMPORTER OFF)
+    set(ASSIMP_BUILD_DXF_IMPORTER OFF)
+    set(ASSIMP_BUILD_CSM_IMPORTER OFF)
+    set(ASSIMP_BUILD_HMP_IMPORTER OFF)
+    set(ASSIMP_BUILD_IRRMESH_IMPORTER OFF)
+    set(ASSIMP_BUILD_IQM_IMPORTER OFF)
+    set(ASSIMP_BUILD_IRR_IMPORTER OFF)
+    set(ASSIMP_BUILD_LWO_IMPORTER OFF)
+    set(ASSIMP_BUILD_LWS_IMPORTER OFF)
+    set(ASSIMP_BUILD_MD2_IMPORTER OFF)
+    set(ASSIMP_BUILD_MD3_IMPORTER OFF)
+    set(ASSIMP_BUILD_MD5_IMPORTER OFF)
+    set(ASSIMP_BUILD_MDC_IMPORTER OFF)
+    set(ASSIMP_BUILD_MDL_IMPORTER OFF)
+    set(ASSIMP_BUILD_NFF_IMPORTER OFF)
+    set(ASSIMP_BUILD_NDO_IMPORTER OFF)
+    set(ASSIMP_BUILD_OFF_IMPORTER OFF)
+    set(ASSIMP_BUILD_OGRE_IMPORTER OFF)
+    set(ASSIMP_BUILD_OPENGEX_IMPORTER OFF)
+    set(ASSIMP_BUILD_PLY_IMPORTER OFF)
+    set(ASSIMP_BUILD_MS3D_IMPORTER OFF)
+    set(ASSIMP_BUILD_COB_IMPORTER OFF)
+    set(ASSIMP_BUILD_BLEND_IMPORTER OFF)
+    set(ASSIMP_BUILD_XGL_IMPORTER OFF)
+    set(ASSIMP_BUILD_Q3D_IMPORTER OFF)
+    set(ASSIMP_BUILD_Q3BSP_IMPORTER OFF)
+    set(ASSIMP_BUILD_RAW_IMPORTER OFF)
+    set(ASSIMP_BUILD_SIB_IMPORTER OFF)
+    set(ASSIMP_BUILD_SMD_IMPORTER OFF)
+    set(ASSIMP_BUILD_STL_IMPORTER OFF)
+    set(ASSIMP_BUILD_TERRAGEN_IMPORTER OFF)
+    set(ASSIMP_BUILD_3D_IMPORTER OFF)
+    set(ASSIMP_BUILD_X_IMPORTER OFF)
+    set(ASSIMP_BUILD_X3D_IMPORTER OFF)
+    set(ASSIMP_BUILD_3MF_IMPORTER OFF)
+    set(ASSIMP_BUILD_MMD_IMPORTER OFF)
+    set(ASSIMP_BUILD OFF)
 
-
-    FetchContent_Declare(
+    FetchContent_Populate(
             assimp
             GIT_REPOSITORY https://github.com/assimp/assimp
-            GIT_TAG v6.0.0
+            GIT_TAG        v6.0.0
+            SOURCE_DIR     ${ASSIMP_ROOT_DIR}
     )
-
-    FetchContent_MakeAvailable(assimp)
+    add_subdirectory(${ASSIMP_ROOT_DIR})
     target_link_libraries(EOS PRIVATE assimp::assimp)
 endmacro()
 
 macro(FETCH_GLM depsDir)
 
-    set(GLM_ROOT_DIR ${depsDir}/src/assimp)
-    FetchContent_Declare(
-            glm
+    set(GLM_ROOT_DIR ${depsDir}/src/glm)
+    FetchContent_Populate(
+            assimp
             GIT_REPOSITORY https://github.com/g-truc/glm
-            GIT_TAG 1.0.1
+            GIT_TAG        1.0.1
+            SOURCE_DIR     ${GLM_ROOT_DIR}
     )
-
-    FetchContent_MakeAvailable(glm)
+    add_subdirectory(${GLM_ROOT_DIR})
     target_link_libraries(EOS PRIVATE glm::glm)
+
+endmacro()
+
+macro(FETCH_TRACY depsDir)
+    set(TRACY_ROOT_DIR ${depsDir}/src/tracy)
+    FetchContent_Populate (
+            tracy
+            GIT_REPOSITORY https://github.com/wolfpld/tracy
+            GIT_TAG master
+            GIT_SHALLOW TRUE
+            SOURCE_DIR     ${TRACY_ROOT_DIR}
+    )
+    add_subdirectory(${TRACY_ROOT_DIR})
+    target_link_libraries(EOS PUBLIC Tracy::TracyClient)
+endmacro()
+
+macro(FETCH_STB depsDir)
+    set(STBI_ROOT_DIR ${depsDir}/src/stb)
+    FetchContent_Populate (
+            fetch_stb
+            GIT_REPOSITORY https://github.com/nothings/stb
+            GIT_SHALLOW TRUE
+            SOURCE_DIR     ${STBI_ROOT_DIR}
+    )
+    include_directories(${STBI_ROOT_DIR})
+endmacro()
+
+macro(FETCH_KTX depsDir)
+    set(KTX_ROOT_DIR ${depsDir}/src/ktx)
+    FetchContent_Populate (
+        fetch_ktx
+        GIT_REPOSITORY https://github.com/KhronosGroup/KTX-Software
+        GIT_TAG v4.4.0
+        GIT_SHALLOW TRUE
+        SOURCE_DIR     ${KTX_ROOT_DIR}
+    )
+    add_subdirectory(${KTX_ROOT_DIR})
+    target_link_libraries(EOS PUBLIC ktx)
 endmacro()
